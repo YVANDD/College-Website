58c4a6a82627f14b6ea4301b7ed9130c145de86b
Yvan <105001832+YVANDD@users.noreply.github.com>
2023-03-31 15:47:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelector('#navLinks');
+
+        function showMenu() {
+        navLinks.style.marginRight = '200px';
+        }
+
+        function hideMenu() {
+        navLinks.style.marginRight = '-200px';
+        }
+
+     
